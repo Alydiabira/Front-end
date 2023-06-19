e45212db66595d1c2366c2a96d7cf3429ff5dbec
Aly <d-aly@outlook.fr>
2023-06-19 09:41:23 +0200

    Update ChatApp.js

--- ReactDEV3G1/src/components/ChatApp/ChatApp.js
+++ ReactDEV3G1/src/components/ChatApp/ChatApp.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import io from 'socket.io-client';
-const socket = io('http://localhost:5000');
+const socket = io('http://localhost:5001');
 
 function ChatApp() {
     const [message, setMessage] = useState('');
