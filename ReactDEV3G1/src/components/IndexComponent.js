71dfd15773a0620a31632fd59476360a8aef38cc
Aly <d-aly@outlook.fr>
2023-07-21 10:03:26 +0200

    Update IndexComponent.js

--- ReactDEV3G1/src/components/IndexComponent.js
+++ ReactDEV3G1/src/components/IndexComponent.js
@@ -1,15 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import HTMLFlipBook from "react-pageflip";
 import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf";
 import "../styles.css";
 import samplePDF from "./Sample-page.pdf";
-
+import { createRoot } from 'react-dom/client';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 const width = 300;
 const height = 450;
-
 const Page = React.forwardRef(({ pageNumber }, ref) => {
   return (
     <div ref={ref}>
@@ -17,7 +15,6 @@ const Page = React.forwardRef(({ pageNumber }, ref) => {
     </div>
   );
 });
-
 function Test() {
   return (
     <Document file={samplePDF}>
@@ -34,5 +31,14 @@ function Test() {
     </Document>
   );
 }
-
-ReactDOM.render(<Test />, document.getElementById("app"));
+// Comment mettre à niveau vers React 18
+//https://react.dev/blog/2022/03/08/react-18-upgrade-guide#updates-to-client-rendering-apis
+//React 18 introduit une nouvelle API racine qui offre une meilleure ergonomie pour la gestion des racines. La nouvelle API racine active également le nouveau moteur de rendu simultané, ce qui vous permet d'activer des fonctionnalités simultanées.
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Test tab="home" />);
+// ReactDOM.createRoot(<Test />, document.getElementById("app"));
+export default Test
+//Résolu - createRoot() Le conteneur cible n'est pas un élément DOM
+// https://www.sharooq.com/solved-createroot-target-container-is-not-a-dom-element
+// Cette erreur se produit généralement en raison de la transmission du mauvais "id" à la méthode "document.getElelementById()" ou de l'appel du script React avant la balise div avec "id" dans le fichier "index.html".
